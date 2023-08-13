37ab66dbfd4cf58657f6e048090332b2b57e454d
raul cubas <raulcubas2016@gmail.com>
2023-08-13 13:29:41 +0100

    :sparkles: add auth model and controller

---
+++ controller/authentication.go
@@ -0,0 +1,63 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/raulcbs/flipcard-api/helper"
+	"github.com/raulcbs/flipcard-api/model"
+)
+
+func Login(context *gin.Context) {
+	var input model.AuthenticationInput
+
+	if err := context.ShouldBindJSON(&input); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := model.FindUserByUsername(input.Username)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = user.ValidatePassword(input.Password)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	jwt, err := helper.GenerateJWT(user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"jwt": jwt})
+}
+
+func Register(context *gin.Context) {
+	var input model.AuthenticationInput
+
+	if err := context.ShouldBindJSON(&input); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user := model.User{
+		Name:     input.Username,
+		Password: input.Password,
+	}
+
+	savedUser, err := user.Save()
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"user": savedUser})
+}
